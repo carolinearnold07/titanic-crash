86b75557ec4c744aa76e788d26f48d092800043e
carolinearnold07 <carolinearnold2026@u.northwestern.edu>
2025-10-31 17:40:55 -0500

    Update install_packages.R

--- src/r-code/install_packages.R
+++ src/r-code/install_packages.R
@@ -1,13 +1,7 @@
-# packages <- c("tidyverse", "caret")
+packages <- c(
+  "tidyverse",
+  "caret"
+)
 
-# for (pkg in packages) {
-#   if (!requireNamespace(pkg, quietly = TRUE)) {
-#     install.packages(pkg, dependencies = FALSE)
-#   } else {
-#     message(paste0(pkg, " is already installed."))
-#   }
-# }
-
-install.packages("pak")
-pak::pkg_install("tidyverse")
-pak::pkg_install("caret")
+install.packages(setdiff(packages, installed.packages()[, "Package"]),
+                 repos = "https://cloud.r-project.org/")
