367734abfbee0f6fd54a07899ad00824569ed1e9
carolinearnold07 <carolinearnold2026@u.northwestern.edu>
2025-10-31 17:32:28 -0500

    Update install_packages.R

--- src/r-code/install_packages.R
+++ src/r-code/install_packages.R
@@ -1,22 +1,9 @@
-#options(
-#  install.packages.compile.from.source = "never",
-#  install.packages.check.source = "no" 
-#)
+packages <- c("tidyverse", "caret")
 
-#packages_to_install <- c("tidyverse", "caret")
-
-#for (pkg in packages_to_install) {
-#  if (!requireNamespace(pkg, quietly = TRUE)) {
-#    install.packages(pkg, repos = "https://cran.rstudio.com/")
-# } else {
-#    message(paste0("Package '", pkg, "' is already installed."))
-#  }
-#}
-
-#if (!requireNamespace("pak", quietly = TRUE)) {
-#  install.packages("pak")
-#}
-
-#pak::pkg_install(c("tidyverse", "caret"))
-
-install.packages(c("tidyverse", "caret"), Ncpus = 4)
+for (pkg in packages) {
+  if (!requireNamespace(pkg, quietly = TRUE)) {
+    install.packages(pkg, dependencies = FALSE)
+  } else {
+    message(paste0(pkg, " is already installed."))
+  }
+}
