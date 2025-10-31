5f6f5fcc481f9a5d34cf51f33100dab5d289663e
carolinearnold07 <carolinearnold2026@u.northwestern.edu>
2025-10-31 17:37:16 -0500

    Update install_packages.R

--- src/r-code/install_packages.R
+++ src/r-code/install_packages.R
@@ -1,9 +1,13 @@
-packages <- c("tidyverse", "caret")
+# packages <- c("tidyverse", "caret")
 
-for (pkg in packages) {
-  if (!requireNamespace(pkg, quietly = TRUE)) {
-    install.packages(pkg, dependencies = FALSE)
-  } else {
-    message(paste0(pkg, " is already installed."))
-  }
-}
+# for (pkg in packages) {
+#   if (!requireNamespace(pkg, quietly = TRUE)) {
+#     install.packages(pkg, dependencies = FALSE)
+#   } else {
+#     message(paste0(pkg, " is already installed."))
+#   }
+# }
+
+install.packages("pak")
+pak::pkg_install("tidyverse")
+pak::pkg_install("caret")
