b2560a99f761bdd7acff3b76ee8e9a7a63a9bc0c
carolinearnold07 <carolinearnold2026@u.northwestern.edu>
2025-10-31 17:18:49 -0500

    Update install_packages.R

--- src/r-code/install_packages.R
+++ src/r-code/install_packages.R
@@ -1,15 +1,20 @@
-options(
-  install.packages.compile.from.source = "never",
-  install.packages.check.source = "no" 
-)
+#options(
+#  install.packages.compile.from.source = "never",
+#  install.packages.check.source = "no" 
+#)
 
-packages_to_install <- c("tidyverse", "caret")
+#packages_to_install <- c("tidyverse", "caret")
 
-# Loop through the packages and install them if not already present
-for (pkg in packages_to_install) {
-  if (!requireNamespace(pkg, quietly = TRUE)) {
-    install.packages(pkg, repos = "https://cran.rstudio.com/")
-  } else {
-    message(paste0("Package '", pkg, "' is already installed."))
-  }
+#for (pkg in packages_to_install) {
+#  if (!requireNamespace(pkg, quietly = TRUE)) {
+#    install.packages(pkg, repos = "https://cran.rstudio.com/")
+# } else {
+#    message(paste0("Package '", pkg, "' is already installed."))
+#  }
+#}
+
+if (!requireNamespace("pak", quietly = TRUE)) {
+  install.packages("pak")
 }
+
+pak::pkg_install(c("tidyverse", "caret"))
