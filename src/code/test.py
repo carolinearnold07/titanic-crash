dc031a156eb1d8037dd5e9db1344cc6514fbe4bc
carolinearnold07 <carolinearnold2026@u.northwestern.edu>
2025-10-31 00:19:51 -0500

    Update script

--- src/code/test.py
+++ src/code/test.py
@@ -4,16 +4,11 @@ import os
 
 print("Hello, world!")
 
-# Get the directory of the current script
-script_dir = os.path.dirname(__file__)
+# Construct the relative path to the CSV
+data_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'gender_submission.csv')
 
-# Construct the relative path to the CSV file
-# We need to go up one level from 'code' to 'src', then down into 'data'
-csv_file_path = os.path.join(script_dir, '..', 'data', 'gender_submission.csv')
+# Read the CSV file into a DataFrame
+df = pd.read_csv(data_path)
 
-# Read the CSV file into a pandas DataFrame
-# df = pd.read_csv(csv_file_path)
-df = pd.read_csv('data/gender_submission.csv')
-
-# You can now work with the DataFrame 'df'
 print(df.head())
+
